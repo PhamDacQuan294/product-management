e6e92ac25682910aa3fdc769f0aa3c2f15222078
quan <quan93909@gmail.com>
2025-06-20 16:09:09 +0700

    Cấu hình folder controllers

--- routes/client/product.route.js
+++ routes/client/product.route.js
@@ -1,14 +1,9 @@
 const express = require("express")
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("client/pages/products/index");
-});
+const controller = require("../../controllers/client/product.controller");
+
+router.get("/", controller.index);
 
 module.exports = router;
 
-/*
-  Bên index.route.js thì app.use("/products", productRoutes); cái này để /products rồi
-  thì bên này ta không để là router.get("/products") được vì nó sẽ nối chuỗi
-  /products/products sai để "/" thôi sau này /products/edit ok
-*/
