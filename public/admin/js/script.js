1654dacfc4ddfb944b843202ab405e1c407512c7
quan <quan93909@gmail.com>
2025-06-21 21:47:15 +0700

    Tính năng lọc theo trạng thái

---
+++ public/admin/js/script.js
@@ -0,0 +1,21 @@
+// Button Status
+// Những thuộc tính mà mình tự định nghĩa thì thêm dấu ngoặc [] vào
+const buttonsStatus = document.querySelectorAll("[button-status]");
+if(buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
+
+  buttonsStatus.forEach(button => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      
+      if(status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+
+      window.location.href = url.href;
+    });
+  });
+}
+// End Button Status
