a5ccdefc288a3a37b35a6fa644feaf795136f8f5
quan <quan93909@gmail.com>
2025-06-22 15:10:47 +0700

    Tối ưu phần Bộ lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,33 +1,12 @@
 const Product = require("../../models/product.model");
 
+const filterStatusHelper = require("../../helpers/filterStatus");
+
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
-  let filterStatus = [
-    {
-      name: "Tat ca",
-      status: "",
-      class: ""
-    },
-    {
-      name: "Hoat dong",
-      status: "active",
-      class: ""
-    },
-    {
-      name: "Dung hoat dong",
-      status: "inactive",
-      class: ""
-    }
-  ];
-
-  if(req.query.status) {
-    const index = filterStatus.findIndex(item => item.status == req.query.status);
-    filterStatus[index].class = "active";
-  } else {
-    const index = filterStatus.findIndex(item => item.status == "");
-    filterStatus[index].class = "active";
-  }
-
+  
+  const filterStatus = filterStatusHelper(req.query);
+  
   let find = {
     deleted: false
   };
