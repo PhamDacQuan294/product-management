797c17e6c47e1ef67e0807bb804382937b3ef228
quan <quan93909@gmail.com>
2025-06-22 15:36:26 +0700

    Tối ưu phần Tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 
 const filterStatusHelper = require("../../helpers/filterStatus");
+const searchHelper = require("../../helpers/search");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -15,14 +16,12 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
 
-  let keyword = "";
+  const objectSearch = searchHelper(req.query);
 
-  if(req.query.keyword) {
-    keyword = req.query.keyword;
-
-    const regex = new RegExp(keyword, "i");
-    find.title = regex;
+  if(objectSearch.regex) {
+    find.title = objectSearch.regex;
   }
+
 
   const products = await Product.find(find);
 
@@ -30,6 +29,6 @@ module.exports.index = async (req, res) => {
     pageTitle: "Danh sach san pham",
     products: products,
     filterStatus: filterStatus,
-    keyword: keyword
+    keyword: objectSearch.keyword
   });
 }
