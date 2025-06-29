1e5b11c89ce810effb55f9cff4ef0c0acf299ab9
quan <quan93909@gmail.com>
2025-06-29 17:58:47 +0700

    Phần thêm mới sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model");
+const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system");
 
@@ -6,6 +7,8 @@ const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
 const redirectHelper = require("../../helpers/redirect");
+
+const createTreeHelper = require("../../helpers/createTree");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -130,8 +133,17 @@ module.exports.deleteItem = async (req, res) => {
 
 // [GET] /admin/products/create
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false
+  };
+  
+  const category = await ProductCategory.find(find);
+
+  const newCategory = createTreeHelper.tree(category);
+
   res.render("admin/pages/products/create", {
-    pageTitle: "Them moi san pham"
+    pageTitle: "Them moi san pham",
+    category: newCategory
   });
 }
 
