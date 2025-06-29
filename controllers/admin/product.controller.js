464d7dd79daf3e8c0aaaa9904636ad637adfd56a
quan <quan93909@gmail.com>
2025-06-29 18:10:50 +0700

    Phần chỉnh sửa sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -177,9 +177,16 @@ module.exports.edit = async (req, res) => {
 
       const product = await Product.findOne(find);
 
+      const category = await ProductCategory.find({
+        deleted: false
+      });
+
+      const newCategory = createTreeHelper.tree(category);
+      
       res.render("admin/pages/products/edit", {
         pageTitle: "Chinh sua san pham",
-        product: product
+        product: product,
+        category: newCategory
       });
   } catch (error) {
     res.redirect(`${systemConfig.prefixAdmin}/products`);
