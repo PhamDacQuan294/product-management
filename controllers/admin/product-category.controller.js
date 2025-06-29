9b20b10d81a99424fd36b6c88e845d67069f83ff
quan <quan93909@gmail.com>
2025-06-29 08:52:33 +0700

    Xây dựng phần quản lý danh mục sản phẩm (Part 2)

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -2,24 +2,54 @@ const ProductCategory = require("../../models/product-category.model");
 
 const systemConfig = require("../../config/system");
 
+const createTreeHelper = require("../../helpers/createTree");
+
 // [GET] /admin/products-category
 module.exports.index = async (req, res) => {
   let find = {
     deleted: false
   };
-  
+
   const records = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.tree(records);
 
   res.render("admin/pages/products-category/index", {
     pageTitle: "Danh muc san pham",
-    records: records
+    records: newRecords
   });
 }
 
 // [GET] /admin/products-category
 module.exports.create = async (req, res) => {
+  let find = {
+    deleted: false
+  };
+
+  // function createTree(arr, parentId = "") {
+  //   const tree = [];
+  //   arr.forEach((item) => {
+  //     if (item.parent_id === parentId) {
+  //       const newItem = item;
+  //       const children = createTree(arr, item.id);
+  //       if(children.length > 0) {
+  //         newItem.children = children;
+  //       }
+  //       tree.push(newItem);
+  //     }
+  //   });
+  //   return tree;
+  // }
+
+  const records = await ProductCategory.find(find);
+
+  const newRecords = createTreeHelper.tree(records);
+
+  // const newRecords = createTree(records);
+
   res.render("admin/pages/products-category/create", {
     pageTitle: "Tao danh muc san pham",
+    records: newRecords
   });
 }
 
