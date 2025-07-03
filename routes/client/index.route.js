4779e3907b32fd198b8209dc198191ee7dc62a11
quan <quan93909@gmail.com>
2025-07-03 20:28:19 +0700

    Hiển thị menu chung bên client
    Tất cả các trang luôn luôn có. Khi tạo trang thì nó có sẵn luôn thì dùng folder middlewares. Có nghĩa là nó phải đi qua thèn trung gian này
    
    Còn folder helper là mình phải nhúng vào từng trang. Khi tạo mới phải nhúng nữa

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,11 @@
+const categoryMiddleware = require("../../middlewares/client/category.middleware");
+
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
 
 module.exports = (app) => {
+  app.use(categoryMiddleware.category); //Luon luon goi cai middeware nay khong can truyen vao tung roters
+
   app.use("/", homeRoutes);
 
   app.use("/products", productRoutes);
