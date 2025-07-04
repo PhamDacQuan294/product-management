83c767172f4d1efe1d1d82e8bc219a8f4ed2fae8
quan <quan93909@gmail.com>
2025-07-04 16:05:08 +0700

    Trang danh sách sản phẩm bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,8 +1,10 @@
-const productsHelper = require("../../helpers/products");
-
-// [GET] /products
+const ProductCategory = require("../../models/product-category.model");
 const Product = require("../../models/product.model");
 
+const productsHelper = require("../../helpers/products");
+const productsCategoryHelper = require("../../helpers/products-category");
+
+// [GET] /products
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
@@ -35,4 +37,46 @@ module.exports.detail = async (req, res) => {
   } catch (error) {
     res.redirect(`/products`);
   }
+}
+
+// [GET] /products/:slug
+module.exports.category = async (req, res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    status: "active",
+    deleted: false
+  });
+
+  // const getSubCategory = async (parentId) => {
+  //   const subs = await ProductCategory.find({
+  //     parent_id: parentId,
+  //     status: "active",
+  //     deleted: false
+  //   });
+
+  //   let allSub = [...subs];
+
+  //   for (const sub of subs) {
+  //     const childs = await getSubCategory(sub.id);
+  //     allSub = allSub.concat(childs);
+  //   }
+
+  //   return allSub;
+  // }
+
+  const listSubCategory = await productsCategoryHelper.getSubCategory(category.id);
+
+  const listSubCategoryId = listSubCategory.map(item => item.id);
+
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId] },
+    deleted: false
+  }).sort({ position: "desc" });
+
+  const newProducts =  productsHelper.priceNewProducts(products)
+
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProducts
+  });
 }
