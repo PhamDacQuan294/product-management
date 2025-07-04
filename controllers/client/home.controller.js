a990f028579b5935c87de13a96a0fe09d6070494
quan <quan93909@gmail.com>
2025-07-04 15:00:00 +0700

    Hiển thị danh sách sản phẩm mới nhất

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -10,11 +10,21 @@ module.exports.index = async (req, res) => {
     status: "active"
   }).limit(6);
 
-  const newProducts = productsHelper.priceNewProducts(productsFeatured);
-  
+  const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
   // End Lay ra san pham noi bat
+
+  // Hien thi danh sach san pham moi nhat
+  const productsNew = await Product.find({
+    deleted: false,
+    status: "active"
+  }).sort({ position: "desc" }).limit(6);
+
+  const newProductsNew = productsHelper.priceNewProducts(productsNew);
+  // End Hien thi danh sach san pham moi nhat
+
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    productsFeatured: newProducts
+    productsFeatured: newProductsFeatured,
+    productsNew: newProductsNew
   });
 };
