438d2c173f859b4e3548b88caba79ea9d297a107
quan <quan93909@gmail.com>
2025-07-04 14:48:35 +0700

    Hiển thị danh sách sản phẩm nổi bật

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,6 +1,20 @@
+const Product = require("../../models/product.model");
+const productsHelper = require("../../helpers/products");
+
 // [GET] /
 module.exports.index = async (req, res) => {
+  // Lay ra san pham noi bat
+  const productsFeatured = await Product.find({
+    featured: "1",
+    deleted: false,
+    status: "active"
+  }).limit(6);
+
+  const newProducts = productsHelper.priceNewProducts(productsFeatured);
+  
+  // End Lay ra san pham noi bat
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    productsFeatured: newProducts
   });
 };
--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,5 @@
+const productsHelper = require("../../helpers/products");
+
 // [GET] /products
 const Product = require("../../models/product.model");
 
@@ -7,10 +9,7 @@ module.exports.index = async (req, res) => {
     deleted: false
   }).sort({ position: "desc" });
 
-  const newProducts = products.map(item => {
-    item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
-    return item;
-  });
+  const newProducts =  productsHelper.priceNewProducts(products)
 
   res.render("client/pages/products/index", {
     pageTitle: "Trang san pham",
