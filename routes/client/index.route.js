c3ca5f7514787a41da90a07da764760f23d514f0
quan <quan93909@gmail.com>
2025-07-05 07:29:52 +0700

    Trang kết quả tìm kiếm

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -2,11 +2,14 @@ const categoryMiddleware = require("../../middlewares/client/category.middleware
 
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
+const searchRoutes = require("./search.route");
 
 module.exports = (app) => {
-  app.use(categoryMiddleware.category); //Luon luon goi cai middeware nay khong can truyen vao tung roters
+  app.use(categoryMiddleware.category);
 
   app.use("/", homeRoutes);
 
   app.use("/products", productRoutes);
+
+  app.use("/search", searchRoutes);
 }
