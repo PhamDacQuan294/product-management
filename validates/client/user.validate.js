6948d7544eaf09ef20e2f2c6137fda6982565d3d
quan <quan93909@gmail.com>
2025-07-07 14:46:24 +0700

    Quên mật khẩu (part 1)

--- validates/client/user.validate.js
+++ validates/client/user.validate.js
@@ -30,3 +30,11 @@ module.exports.loginPost = (req, res, next) => {
 
   next();
 }
+
+module.exports.forgotPasswordPost = (req, res, next) => {
+  if (!req.body.email) {
+    req.flash("error", "Vui lòng nhập email!");
+    return res.redirect("/user/password/forgot");
+  }
+  next();
+}
