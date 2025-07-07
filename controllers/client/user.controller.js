6f8ebafc54c73d2bece1a0ebfd2557c59998cc86
quan <quan93909@gmail.com>
2025-07-07 07:48:03 +0700

    Đăng nhập

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -29,3 +29,43 @@ module.exports.registerPost = async (req, res) => {
 
   res.redirect("/");
 }
+
+// [GET] /user/login
+module.exports.login = async (req, res) => {
+  res.render("client/pages/user/login", {
+    pageTitle:  "Dang nhap tai khoan"
+  });
+}
+
+// [POST] /user/login
+module.exports.loginPost = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = await User.findOne({
+    email: email,
+    deleted: false
+  });
+
+  if(!user) {
+    req.flash("error", "Email khong ton tai!");
+    res.redirect("/user/login");
+    return;
+  }
+
+  if(md5(password) !== user.password) {
+    req.flash("error", "Sai mat khau!");
+    res.redirect("/user/login");
+    return;
+  }
+
+  if(user.status === "inactive") {
+    req.flash("error", "Tai khoan dang bi khoa!");
+    res.redirect("/user/login");
+    return;
+  }
+
+  res.cookie("tokenUser", user.tokenUser);
+  
+  res.redirect("/")
+}
