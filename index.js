f8890954526a7b2d0f3b9e2bfb720f96672d94bc
quan <quan93909@gmail.com>
2025-07-10 08:13:52 +0700

    4.1. Cài đặt
    SocketIO (Lesson 28)

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const flash = require('express-flash');
 const moment = require('moment');
+const http = require('http');
+const { Server } = require("socket.io");
 require("dotenv").config();
 
 const database = require("./config/database");
@@ -19,6 +21,15 @@ database.connect();
 
 const app = express()
 const port = process.env.PORT;
+
+// SocketIO
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+})
+// End SocketIO
 
 app.use(methodOverride('_method'))
 
@@ -58,7 +69,7 @@ app.use((req, res, next) => {
 
 // * ở đây là các trường hợp còn lại 
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
