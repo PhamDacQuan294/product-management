3e086aee10d39b99b9e3352a771c99dd36bf874c
quan <quan93909@gmail.com>
2025-07-13 17:01:53 +0700

    1.4. A hủy kết bạn sẽ xóa A khỏi danh sách Lời mời kết bạn của B

--- public/js/users.js
+++ public/js/users.js
@@ -88,6 +88,7 @@ if (dataUsersAccept) {
       // Vẽ user ra giao diện
       const div = document.createElement("div");
       div.classList.add("col-6");
+      div.setAttribute("user-id", data.infoUserA._id);
 
       div.innerHTML = `
         <div class="box-user">
@@ -125,4 +126,17 @@ if (dataUsersAccept) {
     }
   });
 }
+// End SERVER_RETURN_INFO_ACCEPT_FRIEND
 
+// SERVER_RETURN_USER_ID_CANCEL_FRIEND
+socket.on("SERVER_RETURN_USER_ID_CANCEL_FRIEND", (data) => {
+  const boxUserRemove = document.querySelector(`[user-id='${data.userIdA}']`);
+  if (boxUserRemove) {
+    const dataUsersAccept = document.querySelector("[data-users-accept]");
+    const userIdB = dataUsersAccept.getAttribute("data-users-accept");
+    if(userIdB === data.userIdB) {
+      dataUsersAccept.removeChild(boxUserRemove);
+    }
+  }
+});
+// End SERVER_RETURN_USER_ID_CANCEL_FRIEND
